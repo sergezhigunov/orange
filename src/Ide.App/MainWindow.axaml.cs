5a9043b23a931faad2cdb1741b8fb57b38cf0383
Sergey Zhigunov <serge.zhigunov@mail.ru>
2025-01-17 00:26:03 +0500

    Add application project and minimal UI

---
+++ src/Ide.App/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Orange.Ide.App;
+
+internal partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
---
+++ src/Ide.App/Program.cs
@@ -0,0 +1,16 @@
+﻿using Avalonia;
+
+namespace Orange.Ide.App;
+
+internal static class Program
+{
+    [STAThread]
+    private static void Main(string[] args)
+        => BuildAvaloniaApp()
+            .StartWithClassicDesktopLifetime(args);
+
+    private static AppBuilder BuildAvaloniaApp()
+        => AppBuilder.Configure<App>()
+            .UsePlatformDetect()
+            .LogToTrace();
+}
